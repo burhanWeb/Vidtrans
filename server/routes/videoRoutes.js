25a5e2881d4fa14afd5c84cc7a5a746afda3b9ca
burhan <burhanweb111@gmail.com>
2025-07-31 07:39:29 +0530

    feat(video): add getAllVideos controller and route for fetching user videos

--- server/routes/videoRoutes.js
+++ server/routes/videoRoutes.js
@@ -1,6 +1,7 @@
 import {
   uploadFileToS3,
   DownloadVideo,
+  getAllVideos,
 } from "../controllers/videoControllers.js";
 import { upload } from "../middleware/multerMiddleware.js";
 import ProtectedRoute from "../middleware/protectedRoute.js";
@@ -10,4 +11,5 @@ const router = express.Router();
 
 router.post("/upload", ProtectedRoute, upload.single("video"), uploadFileToS3);
 router.get("/download/:key", DownloadVideo);
+router.get("/allvideos", ProtectedRoute, getAllVideos);
 export default router;
